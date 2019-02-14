55141448b40e70f0d14c76771ae76f6742270e92
Shivam Bansal <shivam2297@gmail.com>
2019-02-14 19:17:11 +0530

    Enabled navigation from home to next fragment with animations.

--- app/src/main/java/com/example/shivam/navigationcomponentdemo/MainActivity.kt
+++ app/src/main/java/com/example/shivam/navigationcomponentdemo/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.shivam.navigationcomponentdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class MainActivity : AppCompatActivity() {
 
--- app/src/main/java/com/example/shivam/navigationcomponentdemo/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/shivam/navigationcomponentdemo/fragments/home/HomeFragment.kt
@@ -6,8 +6,10 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 
 import com.example.shivam.navigationcomponentdemo.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
 /**
@@ -27,5 +29,13 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        next_fragment_btn.setOnClickListener { view ->
+            view.findNavController().navigate(R.id.action_homeFragment_to_nextFragment)
+        }
+
     }
 }
