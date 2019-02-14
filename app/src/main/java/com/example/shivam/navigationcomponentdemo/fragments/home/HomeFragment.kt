f9533465d472a9275eac4c5051f51dc58c6f6f69
developer1 <sshrivastav@contentactive.com>
2019-02-14 16:38:08 +0530

    init commit

---
+++ app/src/main/java/com/example/shivam/navigationcomponentdemo/fragments/home/HomeFragment.kt
@@ -0,0 +1,31 @@
+package com.example.shivam.navigationcomponentdemo.fragments.home
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.shivam.navigationcomponentdemo.R
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class HomeFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
