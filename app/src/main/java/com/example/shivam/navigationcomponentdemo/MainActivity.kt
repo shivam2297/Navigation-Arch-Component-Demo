0dc8bf643f9bce7a05faf6fcb2ee71bcd4047bbc
Shivam Bansal <shivam2297@gmail.com>
2019-03-04 11:07:47 +0530

    Added code to change App bar title with nav component

--- app/src/main/java/com/example/shivam/navigationcomponentdemo/MainActivity.kt
+++ app/src/main/java/com/example/shivam/navigationcomponentdemo/MainActivity.kt
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_home.*
 
@@ -13,8 +15,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findNavController(R.id.navHostFragment).addOnDestinationChangedListener { controller, destination, arguments ->
+        /*findNavController(R.id.navHostFragment).addOnDestinationChangedListener { controller, destination, arguments ->
             this.supportActionBar!!.title = destination.label
-        }
+        }*/
+
+        val navController = findNavController(R.id.navHostFragment)
+        val appBarConfig = AppBarConfiguration(navController.graph)
+
+        toolBar.setupWithNavController(navController, appBarConfig)
     }
 }
